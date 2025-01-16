2ac8eaf824ca74d055da5fb1d19e8ffe62ea90ac
Succes_Lekhuleni <sslekhuleni-2@gmail>
2025-01-16 15:22:19 +0200

    in commit

---
+++ demo/src/main/java/com/enviro/assessment/grad001/succeslekhuleni/demo/student/StudentConfig.java
@@ -0,0 +1,36 @@
+package com.enviro.assessment.grad001.succeslekhuleni.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static java.time.Month.*;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(
+            StudentRepository repository) {
+        return args ->{
+            Student mariam = new Student(
+                    "Mariam",
+                    "mariam.ndlovu@gmail.com",
+                    LocalDate.of(2000, JANUARY, 5)
+            );
+            Student alex = new Student(
+                    "Alex",
+                    "alex@gmail.com",
+                    LocalDate.of(2006, MARCH, 8)
+            );
+
+            repository.saveAll(
+                    List.of(mariam, alex)
+            );
+
+        };
+    }
+}
